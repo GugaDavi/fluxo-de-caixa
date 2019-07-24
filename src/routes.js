ba07ca080b32a5a731be2d096ede3186c0a62afe
GugaDavi <gguedes.tech@gmail.com>
2019-07-24 10:12:15 -0300

    Dependencias

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 
 import CaixaController from './app/controllers/CaixaController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.post('/caixa', CaixaController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
